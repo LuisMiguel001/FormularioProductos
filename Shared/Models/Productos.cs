85a168c4409b6b3b9d24cd71601117955324f626
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2024-02-05 22:28:13 -0400

    Add project files.

---
+++ Shared/Models/Productos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormularioProductos.Shared.Models
+{
+    public class Productos
+    {
+        public string nombre { get; set; }
+
+        public string descripcion { get; set; }
+
+        public DateTime fecha { get; set; }
+
+		public int Cantidad { get; set; }
+
+		public int Precio { get; set; }
+
+		public int TipoProducto { get; set; }
+
+		public int IdSucursal { get; set; }
+	}
+}
